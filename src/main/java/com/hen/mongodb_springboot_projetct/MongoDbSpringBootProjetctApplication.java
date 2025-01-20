2aeabac38ad856cf2cab707b804f8ea58944f00c
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-20 09:15:15 -0300

    Primeiro commit

--- src/main/java/com/hen/mongodb_springboot_projetct/MongoDbSpringBootProjetctApplication.java
+++ src/main/java/com/hen/mongodb_springboot_projetct/MongoDbSpringBootProjetctApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MongoDbSpringBootProjetctApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MongoDbSpringBootProjetctApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MongoDbSpringBootProjetctApplication.class, args);
+    }
 
 }
