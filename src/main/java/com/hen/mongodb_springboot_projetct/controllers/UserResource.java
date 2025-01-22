36feff67d2bbd31392908f20db0c1f5b5e54f07a
HenriqueSilvaIt <hikysilva2@gmail.com>
2025-01-22 10:06:11 -0300

    Usando padrão DTO para retornar usuários

--- src/main/java/com/hen/mongodb_springboot_projetct/controllers/UserResource.java
+++ src/main/java/com/hen/mongodb_springboot_projetct/controllers/UserResource.java
@@ -1,6 +1,7 @@
 package com.hen.mongodb_springboot_projetct.controllers;
 
 import com.hen.mongodb_springboot_projetct.domain.User;
+import com.hen.mongodb_springboot_projetct.dto.UserDTO;
 import com.hen.mongodb_springboot_projetct.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value="/users")
@@ -18,11 +20,13 @@ public class UserResource {
     private UserService userService;
 
     @RequestMapping(method= RequestMethod.GET)
-    public ResponseEntity<List<User>> findAll() { //Response Entity retorna resposta HTTP já formatada com possíveis erros
+    public ResponseEntity<List<UserDTO>> findAll() { //Response Entity retorna resposta HTTP já formatada com possíveis erros
         List<User> list = userService.findAll();
+        // Convertendo o objeto da lista orinal acima para lista DTO:
+        List<UserDTO> listDTO = list.stream().map(u -> new UserDTO(u)).collect(Collectors.toList());
         // da forma acima eu busco a lista no banco de dados
         // e retorno a lista de usuários do banco
-        return ResponseEntity.ok().body(list);// body é onde definimos qual
+        return ResponseEntity.ok().body(listDTO);// body é onde definimos qual
         // vai ser o corpo da reposta, e o .ok() quer dizer
         //que vai retornar ok o HTTP se for tudo com sucesso
     }
